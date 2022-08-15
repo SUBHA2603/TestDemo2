4427b878d4b526bb3a7e16b8fd90b4d6906aea09
amita <sunny3062@gmail.com>
2022-08-15 18:22:40 +0530

    first commit from lead

---
+++ TestDemo2/src/main/java/com/main/resource/NewResource.java
@@ -0,0 +1,11 @@
+package com.main.resource;
+
+public class NewResource {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creating Resource");
+
+	}
+
+}
